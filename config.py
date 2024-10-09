cdd7f5b98ca48c7e969f35f2a5440e7d9d1ae57c
CRYSTAL HOSTING <crystal.hq.hosting@gmail.com>
2024-10-09 13:44:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 env_vars = {
   # Get From my.telegram.org
-  "API_HASH": "",
+  "API_HASH": "7e250aaf4f19530d1debaebace4454bc",
   # Get From my.telegram.org
-  "API_ID": "",
+  "API_ID": "28585290",
   #Get For @BotFather
-  "BOT_TOKEN": "",
+  "BOT_TOKEN": "7366951774:AAHxHivITripwA4HbO76IZg4AKyxe50Wx68",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "",
+  "DATABASE_URL_PRIMARY": "postgresql://postgres:PqlMxuIq3i9sDvvr@quizzically-moving-weimaraner.data-1.use1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
-  "CACHE_CHANNEL": "",
+  "CACHE_CHANNEL": "-1002164639662",
   # Force Subs Channel username without @
-  "CHANNEL": "",
+  "CHANNEL": "-1002238513078",
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
-  # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
-  "FNAME": "",
+  # Ex : Chapter {chap_num} {chap_name} @crystalxmanga_bot
+  "FNAME": "Chapter {chap_num} | {chap_name}  @crystalxmanga_bot",
   # Put Thumb Link 
   "THUMB": ""
 }
