6de856795888645fca1529f8887d570fd1e8053e
CRYSTAL HOSTING <crystal.hq.hosting@gmail.com>
2024-10-29 09:23:54 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,7 +11,7 @@ from .mangahasu import MangaHasuClient
 from .mangabuddy import MangaBuddyClient
 from .asurascans import AsuraScansClient
 from .flamecomics import FlameComicsClient
-from .comick import ComicClient
+from .comick import ComickClient
 from .ninemanga import NineMangaClient
 from .likemanga import LikeMangaClient
 from .mangasee import MangaSeeClient
